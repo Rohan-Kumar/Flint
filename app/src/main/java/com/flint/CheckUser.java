f0ea45a90bbceb58f51a4eecbacecb64db5cae48
darshan <darshan.gowda008@gmail.com>
2016-02-22 15:37:41 +0530

    edited the asyncTask

--- app/src/main/java/com/flint/CheckUser.java
+++ app/src/main/java/com/flint/CheckUser.java
@@ -13,6 +13,7 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by Rohan on 2/18/2016.
@@ -22,7 +23,13 @@ public class CheckUser extends AsyncTask<String, Void, String> {
 
     String Response = "";
 
-    CheckUser() {
+    String URL;
+    ArrayList<paramClass> parameters;
+
+    public CheckUser(String URL, ArrayList<paramClass> parameters) {
+
+        this.URL = URL;
+        this.parameters = parameters;
 
     }
 
@@ -32,16 +39,19 @@ public class CheckUser extends AsyncTask<String, Void, String> {
 
         URL url = null;
         try {
-            url = new URL("http://84.200.84.218:3001/checkUserName");
+            url = new URL(URL);
+
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
             httpURLConnection.setDoInput(true);
             httpURLConnection.setDoOutput(true);
             httpURLConnection.setRequestMethod("POST");
 
-            Uri.Builder builder = new Uri.Builder()
-                    .appendQueryParameter("username", params[0])
-                    .appendQueryParameter("emailid", params[1]);
+            Uri.Builder builder = new Uri.Builder();
+            for(int i=0;i<parameters.size();i++){
+                builder.appendQueryParameter(parameters.get(i).paramName,parameters.get(i).paramValue);
+            }
+
 
 
             String query = builder.build().getEncodedQuery();
@@ -70,6 +80,10 @@ public class CheckUser extends AsyncTask<String, Void, String> {
 
         Log.d("Testing", Response);
         return Response;
+    }
+
+    public class paramClass{
+        String paramName,paramValue;
     }
 
 }
